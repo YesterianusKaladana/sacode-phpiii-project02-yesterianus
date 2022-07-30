71840ea7505dc3ebd35917a341dd83ca5db4af1b
Yesterianus Kaladana <kyesterianus@gmail.com>
2022-07-30 10:50:18 +0900

    add file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sacode-phpiii-project02-yesterianus
+
 Belajar Tipe Data PHP
+
+### Belajar tipe data PHP
+
+    ✅ String
+    ✅ Integer
+    ✅ Float
+    ✅ Array
+    ✅ Boolean
+    ✅ Null
+    ✅ Object
---
+++ array.php
@@ -0,0 +1,12 @@
+<?php 
+	$student = array('Samuel','Nick','Archy', 'Delvi', 'Sepo');
+	foreach($student as $s)
+	{
+		echo 'Nama ' . $s. "<br>";
+	};
+ $kelas = ['php I', 'php II', 'php III'];
+ // var_dump($kelas[0]);
+ //  var_dump($kelas[1]);
+ //  var_dump($kelas[2]);
+
+  echo $kelas[1];
